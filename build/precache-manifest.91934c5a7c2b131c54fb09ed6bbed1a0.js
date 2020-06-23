0c74e3be673e3e8d6848d51efb913d54c4eca7eb
arakhimov <artemiirakhimov@yandex.ru>
2020-06-23 10:17:45 +0300

    project build

---
+++ build/precache-manifest.91934c5a7c2b131c54fb09ed6bbed1a0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beaf09bdfd7dbab88786d1b4d40a38b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9ab8926ac325439b9a53",
+    "url": "/portfolio/static/css/main.7646e273.chunk.css"
+  },
+  {
+    "revision": "a7267c4ff77151eb5731",
+    "url": "/portfolio/static/js/2.6cbc4aa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.6cbc4aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab8926ac325439b9a53",
+    "url": "/portfolio/static/js/main.8f50ce0f.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "a50f9c96a76356e3d01013e0b042989f",
+    "url": "/portfolio/static/media/Comic_Sans_MS.a50f9c96.ttf"
+  },
+  {
+    "revision": "aacb0bb69ff417e4de96eb0886064693",
+    "url": "/portfolio/static/media/HalisGR-Bold.aacb0bb6.otf"
+  },
+  {
+    "revision": "2d38e5b941b55699e49a27dad4fec267",
+    "url": "/portfolio/static/media/Inter-Black.2d38e5b9.woff2"
+  },
+  {
+    "revision": "9b0077c44069dae0d437ac9916d1d051",
+    "url": "/portfolio/static/media/Inter-Regular.9b0077c4.woff2"
+  },
+  {
+    "revision": "9532086ad62b56fbe71706310f9e380a",
+    "url": "/portfolio/static/media/chat_messenger.9532086a.png"
+  },
+  {
+    "revision": "0cf630f45575be8b26966f378ceab0f2",
+    "url": "/portfolio/static/media/cover.0cf630f4.jpg"
+  },
+  {
+    "revision": "f7d9c6cbddbef8925ea980f694e2b0d1",
+    "url": "/portfolio/static/media/cover_dark.f7d9c6cb.jpg"
+  },
+  {
+    "revision": "eadb0b540259e1fc3440b5188f3a49df",
+    "url": "/portfolio/static/media/form_validation.eadb0b54.png"
+  },
+  {
+    "revision": "0d55c97107ecbef19d434ae058368b65",
+    "url": "/portfolio/static/media/learn_to_learn.0d55c971.png"
+  },
+  {
+    "revision": "0bd4fe0a9df360c7273e4fc79fad2fad",
+    "url": "/portfolio/static/media/mesto.0bd4fe0a.png"
+  },
+  {
+    "revision": "398db2baa131a2a832ab98a42fce0a1d",
+    "url": "/portfolio/static/media/news_analyzer.398db2ba.png"
+  },
+  {
+    "revision": "4a8f2e6e52fbed395b1c3bf2f556cc23",
+    "url": "/portfolio/static/media/onpoint_test_task.4a8f2e6e.png"
+  },
+  {
+    "revision": "90a51a8cdd3ce4da1e8505c9915350d5",
+    "url": "/portfolio/static/media/travel_to_russia.90a51a8c.png"
+  }
+]);
